1a45732b2a0e168006da449fa5655627cbe39c6d
Simon-Hubert <s.hubert.mailpro@gmail.com>
2024-09-17 16:10:08 +0200

    Implement FindPlayerStartActorsInArena

--- Source/SmashUE/Private/Match/MatchGameMode.cpp
+++ Source/SmashUE/Private/Match/MatchGameMode.cpp
@@ -2,3 +2,36 @@
 
 
 #include "Match/MatchGameMode.h"
+
+#include "Arena/ArenaPlayerStart.h"
+#include "Kismet/GameplayStatics.h"
+
+void AMatchGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TArray<AArenaPlayerStart*> PlayerStartPoints;
+	FindPlayerStartActorsInArena(PlayerStartPoints);
+	for(AArenaPlayerStart* PlayerStartPoint : PlayerStartPoints)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			3.f,
+			FColor::Cyan,
+			PlayerStartPoint->GetFName().ToString());
+		
+	}
+}
+
+void AMatchGameMode::FindPlayerStartActorsInArena(TArray<AArenaPlayerStart*>& ResultActors)
+{
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AArenaPlayerStart::StaticClass(), FoundActors);
+
+	for(int i=0; i < FoundActors.Num(); i++)
+	{
+		AArenaPlayerStart* ArenaPlayerStartActor = Cast<AArenaPlayerStart>(FoundActors[i]);
+		if (ArenaPlayerStartActor == nullptr) continue;
+		ResultActors.Add(ArenaPlayerStartActor);
+	}
+}
